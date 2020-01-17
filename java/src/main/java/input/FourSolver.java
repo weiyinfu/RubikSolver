9ffb1e27f03116b9787827b21017f4defc0cef6e
weiyinfu <wei.yinfu@qq.com>
2020-01-17 21:30:14 +0800

    代码重构

--- java/src/main/java/input/FourSolver.java
+++
@@ -1,7 +0,0 @@
-package input;
-//四阶魔方求解器
-public class FourSolver {
-public static void main(String[] args) {
-    new WindowInput(new four.FourSolver());
-}
-}
--- java/src/main/java/input/TwoFormulaSolver.java
+++
@@ -1,7 +0,0 @@
-package input;
-//两公式法魔方求解器，这种方法非常冗长繁杂
-public class TwoFormulaSolver {
-public static void main(String[] args) {
-    new WindowInput(new two.TwoFormulaSolver());
-}
-}
